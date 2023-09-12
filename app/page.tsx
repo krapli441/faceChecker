4aa30a59f2f7a40e60037696e1113121efe68e48
박준형 <108733795+krapli441@users.noreply.github.com>
2023-09-12 12:23:39 +0900

    face-api.js가 불러와지지 않는 이유 탐색 중

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 // Next 라이브러리
-import { useRef, useState, ChangeEvent } from "react";
+import { useRef, useState, ChangeEvent, useEffect } from "react";
 
 // CSS
 import styles from "./page.module.css";
@@ -9,7 +9,7 @@ import styles from "./page.module.css";
 import { Box, Button, Text, Input, Heading, Image } from "@chakra-ui/react";
 
 // face-api.js
-import loadModels from "@/components/faceapi";
+import * as faceapi from "face-api.js";
 
 const contentsStyle = {
   display: "flex",
@@ -33,9 +33,49 @@ export default function Home() {
   const [fileName, setFileName] = useState("선택된 파일 없음");
   const hiddenFileInput = useRef<HTMLInputElement>(null);
 
+  useEffect(() => {
+    const loadModels = async () => {
+      await faceapi.nets.tinyFaceDetector.loadFromUri(
+        "https://github.com/justadudewhohacks/face-api.js/weights"
+      );
+    };
+    loadModels();
+  }, []);
+
+  const handleFindFace = async () => {
+    const imageElement = document.getElementById(
+      "uploaded-image"
+    ) as HTMLImageElement;
+
+    if (!imageElement) {
+      console.log("이미지가 로드되지 않았습니다.");
+      return;
+    }
+
+    // // face-api.js 모델 로드 (이 부분은 최초 한 번만 수행되어야 함)
+    // await faceapi.nets.tinyFaceDetector.loadFromUri(
+    //   "https://github.com/justadudewhohacks/face-api.js/weights"
+    // );
+
+    // 얼굴 인식
+    const detections = await faceapi.detectAllFaces(
+      imageElement,
+      new faceapi.TinyFaceDetectorOptions()
+    );
+
+    // 결과 처리
+    if (detections.length > 0) {
+      console.log("인식된 얼굴 수: ", detections.length);
+      console.log("첫 번째 얼굴의 위치: ", detections[0].box);
+    } else {
+      console.log("인식된 얼굴이 없습니다.");
+    }
+  };
+
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
+      setImageSrc(file.name);
       const reader = new FileReader();
       reader.onloadend = () => {
         if (typeof reader.result === "string") {
@@ -70,12 +110,14 @@ export default function Home() {
             style={{ display: "none" }}
           />
           {imageSrc && (
-            <Image boxSize={"150px"} src={imageSrc} alt="Uploaded Preview" />
+            <Image
+              boxSize={"300px"}
+              id="uploaded-image"
+              src={imageSrc}
+              alt="Uploaded Preview"
+            />
           )}
-          <Button
-            colorScheme="blackAlpha"
-            onClick={() => console.log("CLICK!")}
-          >
+          <Button colorScheme="blackAlpha" onClick={() => handleFindFace()}>
             얼굴 찾기
           </Button>
         </Box>
